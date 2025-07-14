b1d9aff1e19f033a0909ad00f7c338c93eeafe69
AidengunUE4 <928954591@qq.com>
2025-07-14 22:42:05 +0800

    Auto commit: batch (批次文件数: 200)

---
+++ Content/Script/SoulslikeFramework/Blueprints/_Characters/parent.lua
@@ -0,0 +1,41 @@
+--
+-- DESCRIPTION
+--
+-- @COMPANY 
+-- @AUTHOR 
+-- @DATE 2025/07/13 00:13:13
+--
+
+
+---@type B_BaseCharacter_C
+local B_BaseCharacter_C = UnLua.Class()
+
+-- function B_BaseCharacter_C:Initialize(Initializer)
+-- end
+
+-- function B_BaseCharacter_C:UserConstructionScript()
+-- end
+
+-- function B_BaseCharacter_C:ReceiveBeginPlay()
+-- end
+
+function B_BaseCharacter_C:Test()
+    print("Parent:Test")
+end
+
+-- function B_BaseCharacter_C:ReceiveEndPlay()
+-- end
+
+-- function B_BaseCharacter_C:ReceiveTick(DeltaSeconds)
+-- end
+
+-- function B_BaseCharacter_C:ReceiveAnyDamage(Damage, DamageType, InstigatedBy, DamageCauser)
+-- end
+
+-- function B_BaseCharacter_C:ReceiveActorBeginOverlap(OtherActor)
+-- end
+
+-- function B_BaseCharacter_C:ReceiveActorEndOverlap(OtherActor)
+-- end
+
+return B_BaseCharacter_C
